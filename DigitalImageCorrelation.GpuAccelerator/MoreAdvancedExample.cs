e0d96050246a6d045d48680bd17536b142780c7b
Michał <dela_m@interia.pl>
2020-10-03 00:36:18 +0200

    feat: first implementation of FindPoint

--- DigitalImageCorrelation.GpuAccelerator/MoreAdvancedExample.cs
+++ DigitalImageCorrelation.GpuAccelerator/MoreAdvancedExample.cs
@@ -6,95 +6,109 @@ namespace DigitalImageCorrelation.GpuAccelerator
     public class MoreAdvancedExample
     {
 
-        public void Mycalculation()
+        ComputeContext context;
+        ComputeEventList events;
+        ComputeKernel kernel;
+        ComputeCommandQueue commands;
+        ComputeProgram program;
+        public MoreAdvancedExample()
         {
-
             ComputeContextPropertyList cpl = new ComputeContextPropertyList(ComputePlatform.Platforms[0]);
-            ComputeContext context = new ComputeContext(ComputeDeviceTypes.Gpu, cpl, null, IntPtr.Zero);
-            ComputeProgram program = new ComputeProgram(context, new string[] { CalculateMyKernel });
+            context = new ComputeContext(ComputeDeviceTypes.Gpu, cpl, null, IntPtr.Zero);
+            program = new ComputeProgram(context, new string[] { FindPoint });
             program.Build(null, null, null, IntPtr.Zero);
-            ComputeCommandQueue commands = new ComputeCommandQueue(context, context.Devices[0], ComputeCommandQueueFlags.None);
-            ComputeEventList events = new ComputeEventList();
-            ComputeKernel kernel = program.CreateKernel("CalculateMyKernel");
-            //byte[,] arr = new byte[,] {
-            //    {0,0},{0,1 },{0,2 },{0,3 },{0,4 },{0,5 },{0,6 },
-            //    {1,0},{1,1 },{1,2 },{1,3 },{1,4 },{1,5 },{1,6 },
-            //    {2,0},{2,1 },{2,2 },{2,3 },{2,4 },{2,5 },{2,6 },
-            //    {3,0},{3,1 },{3,2 },{3,3 },{3,4 },{3,5 },{3,6 },
-            //    {4,0},{4,1 },{4,2 },{4,3 },{4,4 },{4,5 },{4,6 },
-            //    {5,0},{5,1 },{5,2 },{5,3 },{5,4 },{5,5 },{5,6 }
-            //};
-            byte[] arr = new byte[] { 0, 2, 3, 4, 5, 7, 1, 212, 89 };
-            ComputeBuffer<byte> arrBuffer = new ComputeBuffer<byte>(context, ComputeMemoryFlags.ReadWrite | ComputeMemoryFlags.UseHostPointer, arr);
-            kernel.SetMemoryArgument(0, arrBuffer);
-            commands.Execute(kernel, null, new long[] { 10 }, null, events);
+            commands = new ComputeCommandQueue(context, context.Devices[0], ComputeCommandQueueFlags.None);
+            events = new ComputeEventList();
+            kernel = program.CreateKernel("FindPoint");
+        }
+
+        public void Mycalculation(byte[] baseImage, byte[] nextImage, int[] X, int[] Y, int searchDelta, int subsetDelta, int width)
+        {
+            ComputeBuffer<byte> baseImageBuffer = new ComputeBuffer<byte>(context, ComputeMemoryFlags.ReadOnly | ComputeMemoryFlags.UseHostPointer, baseImage);
+            ComputeBuffer<byte> nextImageBuffer = new ComputeBuffer<byte>(context, ComputeMemoryFlags.ReadOnly | ComputeMemoryFlags.UseHostPointer, nextImage);
+            ComputeBuffer<int> XBuffer = new ComputeBuffer<int>(context, ComputeMemoryFlags.ReadWrite | ComputeMemoryFlags.UseHostPointer, X);
+            ComputeBuffer<int> YBuffer = new ComputeBuffer<int>(context, ComputeMemoryFlags.ReadWrite | ComputeMemoryFlags.UseHostPointer, Y);
+            kernel.SetMemoryArgument(0, baseImageBuffer);
+            kernel.SetMemoryArgument(1, nextImageBuffer);
+            kernel.SetMemoryArgument(2, XBuffer);
+            kernel.SetMemoryArgument(3, YBuffer);
+            kernel.SetValueArgument(4, searchDelta);
+            kernel.SetValueArgument(5, subsetDelta);
+            kernel.SetValueArgument(6, width);
+            commands.Execute(kernel, null, new long[] { X.Length }, null, events);
             commands.Finish();
         }
 
-
-
-        static string CalculateMyKernel
+        static string FindPoint
         {
             get
             {
                 return @"
-            kernel void CalculateMyKernel(global int* arr) 
+            kernel void FindPoint(global char* baseImage, global char* nextImage, global int* X, global int* Y, int searchDelta,int subsetDelta, int width) 
             {
                 int id = get_global_id(0);
-                arr[id] = arr[id]/2;              
-            }";
-            }
-        }
-
-
-
-
-        public double[][] CalculateGPU(int partitionSize, double[][] sets, int period)
-        {
-            ComputeContextPropertyList cpl = new ComputeContextPropertyList(ComputePlatform.Platforms[0]);
-            ComputeContext context = new ComputeContext(ComputeDeviceTypes.Gpu, cpl, null, IntPtr.Zero);
-            ComputeProgram program = new ComputeProgram(context, new string[] { CalculateKernel });
-            program.Build(null, null, null, IntPtr.Zero);
-            ComputeCommandQueue commands = new ComputeCommandQueue(context, context.Devices[0], ComputeCommandQueueFlags.None);
-            ComputeEventList events = new ComputeEventList();
-            ComputeKernel kernel = program.CreateKernel("Calc");
-            double[][] results = new double[sets.Length][];
-            double periodFactor = 2d / (1d + period);
-            int offset = 0;
-            int first = offset;
-            int last = Math.Min(offset + partitionSize, sets.Length);
-            int length = last - first;
-            ComputeBuffer<int> offsetBuffer = new ComputeBuffer<int>(context, ComputeMemoryFlags.ReadWrite | ComputeMemoryFlags.UseHostPointer, offset);
-            ComputeBuffer<int> lengthsBuffer = new ComputeBuffer<int>(context, ComputeMemoryFlags.ReadWrite | ComputeMemoryFlags.UseHostPointer, length);
-            kernel.SetMemoryArgument(0, offsetBuffer);
-            kernel.SetMemoryArgument(1, lengthsBuffer);
-            //kernel.SetMemoryArgument(2, doublesBuffer);
-            kernel.SetValueArgument(3, periodFactor);
-            //executeStopWatch.Start();
-            //commands.Execute(kernel, null, new long[] { merged.Lengths.Length }, null, events);
-            return results;
-        }
-
-        static string CalculateKernel
-        {
-            get
-            {
-                return @"
-            kernel void Calc(global int* offsets, global int* lengths, global double* doubles, double periodFactor) 
-            {
-                int id = get_global_id(0);
-                int start = offsets[id];
-                int length = lengths[id];
-                int end = start + length;
-                double sum = doubles[start];
-
-                for(int i = start; i < end; i++)
+                int dx = 0;
+                int dy = 0;
+                int diff = 2147483647;
+                for (int v = -searchDelta; v <= searchDelta; v++)
                 {
-                    sum = sum + periodFactor * ( doubles[i] - sum );
-                    doubles[i] = sum;
+                    for (int u = -searchDelta; u <= searchDelta; u++)
+                    {
+                        int sum = 0;
+                        for (int y = -subsetDelta; y <= subsetDelta; y++)
+                        {
+                            for (int x = -subsetDelta; x <= subsetDelta; x++)
+                            {
+                                int v0 = baseImage[(Y[id] + y) * width + X[id] + x];
+                                int v1 = nextImage[(Y[id] + y + v) * width + X[id] + x + u];
+                                sum += (v0 - v1) * (v0 - v1);
+                            }
+                        }
+                        if (sum < diff)
+                        {
+                            diff = sum;
+                            dx = u;
+                            dy = v;
+                        }
+                    }
                 }
+                X[id] += dx;
+                Y[id] += dy;
             }";
             }
+        }
+
+
+        private void FindVertex(int searchDelta, int subsetDelta, byte[] baseImage, byte[] nextImage, int[] X, int[] Y, int width)
+        {
+            int id = 0;
+            int dx = 0;
+            int dy = 0;
+            int diff = int.MaxValue;
+            for (int v = -searchDelta; v <= searchDelta; v++)
+            {
+                for (int u = -searchDelta; u <= searchDelta; u++)
+                {
+                    int sum = 0;
+                    for (int y = -subsetDelta; y <= subsetDelta; y++)
+                    {
+                        for (int x = -subsetDelta; x <= subsetDelta; x++)
+                        {
+                            int v0 = baseImage[(Y[id] + y) * width + X[id] + x];
+                            int v1 = nextImage[(Y[id] + y + v) * width + X[id] + x + u];
+                            sum += (v0 - v1) * (v0 - v1);
+                        }
+                    }
+                    if (sum < diff)
+                    {
+                        diff = sum;
+                        dx = u;
+                        dy = v;
+                    }
+                }
+            }
+            X[id] += dx;
+            Y[id] += dy;
         }
     }
 }
--- DigitalImageCorrelation.Tests/TestMoreAdvancedExample.cs
+++ DigitalImageCorrelation.Tests/TestMoreAdvancedExample.cs
@@ -20,9 +20,15 @@ namespace DigitalImageCorrelation.Tests
         [Test]
         public void PrimeGPUTest()
         {
-            //int[] Primes = Enumerable.Range(2, number).ToArray();
+            byte[] baseImage = new byte[] { 0, 2, 3, 4, 5, 7, 1, 200, 89, 150 };
+            byte[] nextImage = new byte[] { 1, 3, 4, 4, 23, 2, 12, 220, 9, 15 }; ;
+            int[] X = new int[baseImage.Length];
+            int[] Y = new int[baseImage.Length];
+            int searchDelta = 5;
+            int subsetDelta = 6;
+            int width = 3;
             sw.Start();
-            gpu.Mycalculation();
+            gpu.Mycalculation(baseImage, nextImage, X, Y, searchDelta, subsetDelta, width);
             sw.Stop();
             Console.WriteLine($"PrimeGPUTest: {0} - {sw.ElapsedMilliseconds} ms");
         }
