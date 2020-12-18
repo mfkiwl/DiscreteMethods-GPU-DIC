cb7d1c0dfffb623aa94d43c642b448d0b35582f1
Michał <dela_m@interia.pl>
2020-12-18 18:07:17 +0100

    chore: remove warnings, x64 build

--- DigitalImageCorrelation.Desktop/Drawing/ResultPainter/PointResultPainter.cs
+++ DigitalImageCorrelation.Desktop/Drawing/ResultPainter/PointResultPainter.cs
@@ -1,5 +1,4 @@
 ﻿using DigitalImageCorrelation.Desktop.Requests;
-using System;
 using System.Drawing;
 
 namespace DigitalImageCorrelation.Desktop.Drawing.ResultPainter
@@ -10,26 +9,15 @@ namespace DigitalImageCorrelation.Desktop.Drawing.ResultPainter
         private readonly Pen _elipsePen = new Pen(Color.FromArgb(200, 255, 0, 0), 1);
         public Bitmap Paint(Bitmap bitmap, DrawRequest request)
         {
-            try
+            if (request.AnalyzeResults != null && request.AnalyzeResults.ImageResults.ContainsKey(request.Image.Index))
             {
-                if (request.AnalyzeResults != null && request.AnalyzeResults.ImageResults.ContainsKey(request.Image.Index))
+                var result = request.AnalyzeResults.ImageResults[request.Image.Index];
+                var g = Graphics.FromImage(bitmap);
+                foreach (var point in result.Vertexes)
                 {
-                    var result = request.AnalyzeResults.ImageResults[request.Image.Index];
-                    var g = Graphics.FromImage(bitmap);
-                    foreach (var point in result.Vertexes)
-                    {
-                        g.DrawRectangle(_rectanglePen, new Rectangle(point.X - request.SubsetDelta, point.Y - request.SubsetDelta, request.SubsetDelta * 2, request.SubsetDelta * 2));
-                        g.DrawEllipse(_elipsePen, new Rectangle(point.X, point.Y, 1, 1));
-                    }
+                    g.DrawRectangle(_rectanglePen, new Rectangle(point.X - request.SubsetDelta, point.Y - request.SubsetDelta, request.SubsetDelta * 2, request.SubsetDelta * 2));
+                    g.DrawEllipse(_elipsePen, new Rectangle(point.X, point.Y, 1, 1));
                 }
-                return bitmap;
-            }
-            catch (Exception ex)
-            {
-
-                var tmp = 0;
-
-
             }
             return bitmap;
         }
